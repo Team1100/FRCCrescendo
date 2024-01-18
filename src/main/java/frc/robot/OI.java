de442a446b2f5f97fa7f0d9e1ed6fb26b257d4a3
Mark Hecker <markbh@verizon.net>
2024-01-17 20:45:50 -0500

    adds TestingDashboard framework and OI.

---
+++ src/main/java/frc/robot/OI.java
@@ -0,0 +1,57 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2018-2019 FIRST. All Rights Reserved.                        */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
+package frc.robot;
+
+import edu.wpi.first.wpilibj.XboxController;
+
+/**
+ * This class is the glue that binds the controls on the physical operator
+ * interface to the commands and command groups that allow control of the robot.
+ */
+public class OI {
+  private static OI m_oi;
+
+  private static XboxController m_DriverXboxController;
+  private static XboxController m_OperatorXboxController;
+
+  /**
+   * Used outside of the OI class to return an instance of the class.
+   * @return Returns instance of OI class formed from constructor.
+   */
+  public static OI getInstance() {
+    if (m_oi == null) {
+      m_oi = new OI();
+    }
+    return m_oi;
+  }
+
+  public OI() {
+    // User Input
+    // TODO: Tune deadband
+    m_DriverXboxController = new XboxController(RobotMap.U_DRIVER_XBOX_CONTROLLER);
+    m_OperatorXboxController = new XboxController(RobotMap.U_OPERATOR_XBOX_CONTROLLER);
+    
+    ////////////////////////////////////////////////////
+    // Now Mapping Commands to XBox
+    ////////////////////////////////////////////////////
+    m_DriverXboxController.getAButton();
+    m_OperatorXboxController.getAButton();
+  }
+
+  /**
+   * Returns the Xbox Controller
+   * @return the Xbox Controller
+   */
+  public XboxController getDriverXboxController() {
+      return m_DriverXboxController;
+  }
+
+  public XboxController getOperatorXboxController() {
+    return m_OperatorXboxController;
+  }  
+}
