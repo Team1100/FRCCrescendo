8dfcfa78eedd3561ea3f8afe84d18daf37aa938a
AshleyY06 <ash.windflower@gmail.com>
2024-01-20 11:01:54 -0500

    debugs Consume and Expel commands
    This commit debugs the Consume and Expel commands,
    so the OI isn't called twice.
    It also fixes the direction of the motors.

--- src/main/java/frc/robot/commands/Intake/Consume.java
+++ src/main/java/frc/robot/commands/Intake/Consume.java
@@ -4,22 +4,17 @@
 
 package frc.robot.commands.Intake;
 
-
-import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj2.command.Command;
 
-import frc.robot.OI;
 import frc.robot.subsystems.Intake;
 import frc.robot.testingdashboard.TestingDashboard;
 
 public class Consume extends Command {
-  private static XboxController m_driverController;
   Intake m_intake;
 
-  /** Creates a new ArcadeDrive. */
+  /** Creates a new Consume. */
   public Consume() {
     m_intake = Intake.getInstance();
-    m_driverController = OI.getInstance().getDriverXboxController();
 
     addRequirements(m_intake);
   }
--- src/main/java/frc/robot/commands/Intake/Expel.java
+++ src/main/java/frc/robot/commands/Intake/Expel.java
@@ -5,22 +5,17 @@
 
 package frc.robot.commands.Intake;
 
-
-import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj2.command.Command;
 
-import frc.robot.OI;
 import frc.robot.subsystems.Intake;
 import frc.robot.testingdashboard.TestingDashboard;
 
 public class Expel extends Command {
-  private static XboxController m_driverController;
   Intake m_intake;
 
-  /** Creates a new ArcadeDrive. */
+  /** Creates a new Expel. */
   public Expel() {
     m_intake = Intake.getInstance();
-    m_driverController = OI.getInstance().getDriverXboxController();
 
     addRequirements(m_intake);
   }
