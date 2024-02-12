95d32e8d84362077f6a85c3027ee5de2050bb52a
Mark Hecker <markbh@verizon.net>
2024-02-12 17:29:23 -0500

    adds CommandGroup base classes to testingdashboard
    Creates ParallelCommandGroup, ParallelDeadlineGroup, ParallelRaceGroup, and
    SequentialCommandGroup.
    By default, commands are added to PathPlanner.

---
+++ src/main/java/frc/robot/testingdashboard/ParallelCommandGroup.java
@@ -0,0 +1,28 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.testingdashboard;
+
+import com.pathplanner.lib.auto.NamedCommands;
+
+// NOTE:  Consider using this command inline, rather than writing a subclass.  For more
+// information, see:
+// https://docs.wpilib.org/en/stable/docs/software/commandbased/convenience-features.html
+public class ParallelCommandGroup extends edu.wpi.first.wpilibj2.command.ParallelCommandGroup {
+
+  public ParallelCommandGroup(SubsystemBase subsystem, String groupName, String name, boolean registerNamedCommand) {
+    super();
+    setName(name);
+    TestingDashboard.getInstance().registerCommand(subsystem.getName(), groupName, this);
+    if (registerNamedCommand) {
+      NamedCommands.registerCommand(name, this);
+    }
+  }
+
+  public ParallelCommandGroup(SubsystemBase subsystem, String groupName, String name) {
+    this(subsystem, groupName, name, true);
+  }
+}
+
+
