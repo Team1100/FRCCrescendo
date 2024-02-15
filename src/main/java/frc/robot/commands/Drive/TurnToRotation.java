e15e2ecaa2156cf4857c48feb2166e8b38495a9b
Brian Hecker <bhecker@exagrid.com>
2024-02-15 13:10:13 -0500

    adds turning commands
    This commit adds new commands for turning to a specified angle, and turning toward a specified target.

--- src/main/java/frc/robot/commands/Drive/TurnToRotation.java
+++ src/main/java/frc/robot/commands/Drive/TurnToRotation.java
@@ -4,11 +4,13 @@
 
 package frc.robot.commands.Drive;
 
+import java.util.function.Supplier;
+
+import edu.wpi.first.math.MathUtil;
 import edu.wpi.first.math.controller.ProfiledPIDController;
 import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.math.geometry.Rotation2d;
-import edu.wpi.first.math.geometry.Transform2d;
-import edu.wpi.first.math.geometry.Translation2d;
+import edu.wpi.first.math.kinematics.ChassisSpeeds;
 import frc.robot.Constants;
 import frc.robot.subsystems.Drive;
 import frc.robot.testingdashboard.Command;
@@ -16,18 +18,21 @@ import frc.robot.testingdashboard.TDNumber;
 
 public class TurnToRotation extends Command {
   protected Drive m_drive;
-  protected Rotation2d m_targetRot;
+  protected Supplier<Rotation2d> m_targetSupplier;
+  private Rotation2d m_targetRot;
   private ProfiledPIDController m_thetaController;
+  
+  TDNumber m_TargetAngle;
 
-  public TurnToRotation(Rotation2d target) {
-    this(target, "Auto Commands", "TurnToTarget");
+  public TurnToRotation(Supplier<Rotation2d> target) {
+    this(target, "Auto Commands", "TurnToRotation");
   }
 
   //Used for subclasses so they can set they're own testing dashboard name
-  protected TurnToRotation(Rotation2d target, String groupName, String name){
+  protected TurnToRotation(Supplier<Rotation2d> targetSupplier, String groupName, String name){
     super(Drive.getInstance(), groupName, name);
     m_drive = Drive.getInstance();
-    m_targetRot = target;
+    m_targetSupplier = targetSupplier;
 
 
     m_thetaController = new ProfiledPIDController(
@@ -35,6 +40,8 @@ public class TurnToRotation extends Command {
     m_thetaController.enableContinuousInput(-Math.PI, Math.PI);
 
     m_thetaController.setTolerance(0.1);
+
+    m_TargetAngle = new TDNumber(m_drive, "Test Outputs", "OutputAngle");
  
     addRequirements(m_drive);
   }
@@ -42,7 +49,8 @@ public class TurnToRotation extends Command {
   // Called when the command is initially scheduled.
   @Override
   public void initialize() {
-
+    m_targetRot = m_targetSupplier.get();
+    m_TargetAngle.set(m_targetRot.getDegrees());
   }
 
   // Called every time the scheduler runs while the command is scheduled.
@@ -50,8 +58,12 @@ public class TurnToRotation extends Command {
   public void execute() {
     Pose2d currentPose = m_drive.getPose();
 
-    double rotation = m_thetaController.calculate(currentPose.getRotation().getRadians(), m_targetRot.getRadians());
-    m_drive.drive(0,0, rotation, true, false);
+    double rotation = m_thetaController.calculate(
+        MathUtil.angleModulus(currentPose.getRotation().getRadians()),
+        MathUtil.angleModulus(m_targetRot.getRadians()));
+
+    ChassisSpeeds speeds = new ChassisSpeeds(0, 0, rotation);
+    m_drive.drive(speeds);
   }
 
   // Called once the command ends or is interrupted.
