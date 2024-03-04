477f03adc6b3240981fc647e944a25a067b62bab
Calvin Midgley <calvin.midgley@gmail.com>
2024-03-04 17:36:59 -0500

    removed pivottospeaker stoppivot

--- src/main/java/frc/robot/commands/BarrelPivot/PivotToSpeaker.java
+++ src/main/java/frc/robot/commands/BarrelPivot/PivotToSpeaker.java
@@ -61,9 +61,7 @@ public class PivotToSpeaker extends Command {
 
   // Called once the command ends or is interrupted.
   @Override
-  public void end(boolean interrupted) {
-    m_barrelPivot.stopPivot();
-  }
+  public void end(boolean interrupted) {}
 
   // Returns true when the command should end.
   @Override
