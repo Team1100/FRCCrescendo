15a479b5655ee989e58bfece30c71b0889763f47
AshleyY06 <ash.windflower@gmail.com>
2024-03-13 22:26:43 -0400

    updates autos
    This commit updates the current autos that the robot has. Rather than
    keeping the note stuck in the Intake, it is moved into the Barrel which
    subsequently allows for faster shooting. To combat the robot drifting,
    the P for the translation & rotation PID was reduced from 5.0 to 1.0.
    This commit also sends the AutoChooser to SmartDashboard, so that it
    appears on the Driver Station.
    
    Test the PID again with access to AprilTags at WPI***

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -78,6 +78,7 @@ import com.pathplanner.lib.auto.AutoBuilder;
 import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
 import edu.wpi.first.wpilibj2.command.button.Trigger;
@@ -149,6 +150,7 @@ public class RobotContainer {
 
     // Create Testing Dashboard
     TestingDashboard.getInstance().createTestingDashboard();
+    SmartDashboard.putData(m_autoChooser);
   }
 
   private void registerCommands() {
