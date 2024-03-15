6888cd434f63d86667807b561f0c53c87a2902d4
Caitlin270 <148595191+Caitlin270@users.noreply.github.com>
2024-03-15 14:26:15 -0400

    fixes auto lights
    This codes makes the auto lights less glitchy.

--- src/main/java/frc/robot/subsystems/Lights.java
+++ src/main/java/frc/robot/subsystems/Lights.java
@@ -89,14 +89,14 @@ public class Lights extends SubsystemBase {
     for (var i = 0; i < m_LEDBuffer.getLength(); i++) {
       // Calculate the hue - hue is easier for rainbows because the color
       // shape is a circle so only one value needs to precess
-      final var hue = 60 + (m_rainbowFirstPixelHue + (i * 120 / m_LEDBuffer.getLength())) % 82;
+      final var hue = 60 + (m_rainbowFirstPixelHue + (i * 130 / m_LEDBuffer.getLength())) % 85;
       // Set the value
       m_LEDBuffer.setHSV(i, hue, 255, 128);
     }
     // Increase by to make the rainbow "move"
     m_rainbowFirstPixelHue += 2;
     // Check bounds
-    m_rainbowFirstPixelHue %= 60;
+    m_rainbowFirstPixelHue %= 90;
   }
 
     public void makeWarm() {
